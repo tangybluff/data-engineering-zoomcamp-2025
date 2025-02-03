9ce6ba32626d4abcab8cad5c335d2579f2f18a32
tangybluff <ahmedbouallou@gmail.com>
2025-02-04 00:35:55 +0100

    Commit important files for the week 1 project

---
+++ week_1/2_docker_sql/pipeline.py
@@ -0,0 +1,11 @@
+import sys
+
+import pandas as pd
+
+print(sys.argv)
+day = sys.argv[1]
+
+
+# some fancy stuff with pandas like loading csv file
+
+print(f'Job completed successfully for day = {day}.')
