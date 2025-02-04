860f121f87a6898c8d00bdc56ef705221cc7c49d
tangybluff <ahmedbouallou@gmail.com>
2025-02-04 12:46:30 +0100

    Py data modifications

---
+++ week_1/2_docker_sql/ingest_data.py
@@ -0,0 +1,33 @@
+from time import time
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+engine = create_engine('postgresql://root:root@localhost:5431/ny_taxi')
+
+df_iter = pd.read_csv('yellow_tripdata_2021-01.csv', iterator=True, chunksize=100000)
+
+df = next(df_iter)
+
+df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+df.head(n=0).to_sql(name='yellow_taxi_data', con=engine, if_exists='replace')
+
+df.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')
+
+while True: 
+    t_start = time()
+
+    df = next(df_iter)
+
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    
+    df.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')
+
+    t_end = time()
+
+    print('Additional chunk inserted, took %.3f second(s)' % (t_end - t_start))
+
+
