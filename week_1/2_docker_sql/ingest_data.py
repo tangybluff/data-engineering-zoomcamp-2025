c24299b49b90d7da0c13209af952b22848499a6e
tangybluff <ahmedbouallou@gmail.com>
2025-02-05 02:44:44 +0100

    Ingest python script test

--- week_1/2_docker_sql/ingest_data.py
+++ week_1/2_docker_sql/ingest_data.py
@@ -1,33 +1,63 @@
+
+import os
+import argparse 
+
 from time import time
 
 import pandas as pd
 from sqlalchemy import create_engine
 
-engine = create_engine('postgresql://root:root@localhost:5431/ny_taxi')
+def main(params):
+    user = params.user
+    password = params.password
+    host = params.host
+    port = params.port
+    db = params.db
+    table_name = params.table_name
+    url = params.url
+    csv_name = 'output.csv.gz'
 
-df_iter = pd.read_csv('yellow_tripdata_2021-01.csv', iterator=True, chunksize=100000)
+# Download the CSV file
+    os.system(f"wget {url} -O {csv_name}")
 
-df = next(df_iter)
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
-df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-
-df.head(n=0).to_sql(name='yellow_taxi_data', con=engine, if_exists='replace')
-
-df.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')
-
-while True: 
-    t_start = time()
+    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
 
     df = next(df_iter)
 
     df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
     df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+
+    df.to_sql(name=table_name, con=engine, if_exists='append')
+
+    while True: 
+        t_start = time()
+
+        df = next(df_iter)
+
+        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
     
-    df.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')
+        df.to_sql(name=table_name, con=engine, if_exists='append')
 
-    t_end = time()
+        t_end = time()
 
-    print('Additional chunk inserted, took %.3f second(s)' % (t_end - t_start))
+        print('Additional chunk inserted, took %.3f second(s)' % (t_end - t_start))
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Ingest CSV data into Postgres')
 
+    parser.add_argument('--user', help='username for Postgres')
+    parser.add_argument('--password', help='password for Postgres')
+    parser.add_argument('--host', help='host for Postgres')
+    parser.add_argument('--port', help='port for Postgres')
+    parser.add_argument('--db', help='Postgres database name')
+    parser.add_argument('--table_name', help='Table where results will be stored')
+    parser.add_argument('--url', help='URL of the CSV file')
+
+    args = parser.parse_args()
+
+    main(args)
