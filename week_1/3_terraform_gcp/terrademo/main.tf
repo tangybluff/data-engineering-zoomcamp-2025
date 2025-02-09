c3b3184f04402f4943f0534933ebaa2adb3001b2
tangybluff <ahmedbouallou@gmail.com>
2025-02-09 23:50:34 +0100

    Modified main.tf file with terraform fmt

--- week_1/3_terraform_gcp/terrademo/main.tf
+++ week_1/3_terraform_gcp/terrademo/main.tf
@@ -1,13 +1,13 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "6.19.0"
     }
   }
 }
 
 provider "google" {
-  project     = "terraform-demo-450422"
-  region      = "us-central1"
+  project = "terraform-demo-450422"
+  region  = "us-central1"
 }
