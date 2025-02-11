7162a9cbdfdff402b9e60a6689aa4138d850388a
tangybluff <ahmedbouallou@gmail.com>
2025-02-11 18:56:17 +0100

    Modified main.tf; edited gitignore for clean commits

--- week_1/3_terraform_gcp/terrademo/main.tf
+++ week_1/3_terraform_gcp/terrademo/main.tf
@@ -16,3 +16,23 @@ provider "google" {
 
 # run terraform init to download the provider
 # Next step will be to create a bucket
+
+# Variable in resource has to be globally unique; this means that the bucket name has to be unique across all Google Cloud Storage buckets
+# Name of the bucket can be project specific, for example, project name + bucket name or terraform will generate a random name
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "terraform-demo-450422-terra-bucket"
+  location      = "EU"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1   
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
+
+# terraform plan to see the changes
+# terraform apply to apply the changes
