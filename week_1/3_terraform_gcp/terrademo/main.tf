872decde33490dd2a4b976e460c22846282ab47b
tangybluff <ahmedbouallou@gmail.com>
2025-02-11 18:18:30 +0100

    Modified main.tf; removed hardcode

--- week_1/3_terraform_gcp/terrademo/main.tf
+++ week_1/3_terraform_gcp/terrademo/main.tf
@@ -7,8 +7,9 @@ terraform {
   }
 }
 
+# export GOOGLE_CREDENTIALS="path/to/your/credentials.json"
+# echo $GOOGLE_CREDENTIALS
 provider "google" {
-    credentials = "./keys/my-creds.json"
   project = "terraform-demo-450422"
   region  = "europe-southwest1"
 }
